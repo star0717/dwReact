32e35b409ca0f6a942f58b2ecc342d4a265134b1
star0717 <ydb0717@gmail.com>
2024-07-02 10:12:25 +0900

    commit

---
+++ dwReact/myreact2-main/src/App.js
@@ -0,0 +1,11 @@
+import { Index } from "./components/Index";
+
+function App() {
+  return (
+    <>
+      <Index />
+    </>
+  );
+}
+
+export default App;
---
+++ dwReact/myreact2-main/src/components/Index.js
@@ -0,0 +1,32 @@
+export function Index() {
+  return (
+    <>
+      <div className="container">
+        <div className="title">
+          <h1>GAME SHOP</h1>
+        </div>
+        <div className="menu">
+          <div>
+            <a href="index.html">HOME</a>
+          </div>
+          <div>
+            <a href="board.html">BOARD</a>
+          </div>
+          <div>
+            <a href="mypage.html">MY PAGE</a>
+          </div>
+          <div>
+            <a href="login.html">LOGIN</a>
+          </div>
+          <div>
+            <a href="cart.html">CART</a>
+          </div>
+        </div>
+        <div className="content"></div>
+        <div className="footer">
+          <h3>FOOTER</h3>
+        </div>
+      </div>
+    </>
+  );
+}
